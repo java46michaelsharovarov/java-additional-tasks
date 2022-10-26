5ac97804773e91142d6a3237b07689eb66c4859b
Mik <m.shac86@gmail.com>
2022-10-26 22:57:28 +0300

    task_32 without combiner

--- src/msh/util/MaxRatedWinnersCollector.java
+++ src/msh/util/MaxRatedWinnersCollector.java
@@ -6,59 +6,25 @@ import java.util.function.ToLongFunction;
 import java.util.stream.Collector;
 import java.util.stream.Collector.Characteristics;
 
-/**
- * This collector collects only those elements of Stream<T> 
- * which have the maximal rate due to specified 'rater' function.
- * The collector is built using static method MaxRatedWinnersCollector.of(...)
- */
 public class MaxRatedWinnersCollector {
 	private MaxRatedWinnersCollector() {} // hidden constructor
 
-	/**
-	 * Constructs the Collector using specified 'rater' instance
-	 */
-	public static <T> Collector<T, ?, List<T>> of(ToLongFunction<T> rater) {
+	public static <T> Collector<T, ?, RateAccumulator<T>> of(ToLongFunction<T> rater) {
 		return Collector.of(
 				() -> new RateAccumulator<T>(rater), 
 				RateAccumulator::accumulate, 
 				RateAccumulator::combine, 
-				RateAccumulator::getWinnersList, 
 				Characteristics.UNORDERED);
 	}
-
-	/**
-	 * The internal accumulating class for implementation of Collector
-	 */
-	private static class RateAccumulator<T> {
+	static class RateAccumulator<T> {
 		private ToLongFunction<T> rater;
 		private LinkedList<T> currentWinnersList = new LinkedList<>();
 		private long currentMax;
 
-		/**
-		 * Constructor
-		 * @param <T> rater - functional object, giving the long 'rate' value to each T object
-		 */
 		public RateAccumulator(ToLongFunction<T> rater) {
 			this.rater = rater;
 		}
 
-		/**
-		 * Gets list of collected 'winners' - the rate champions
-		 * @return returns list of winners (the T instances having max rate)
-		 */
-		public List<T> getWinnersList() {
-			return currentWinnersList;
-		}
-
-		/**
-		 * Accumulate 'champions', using the following algorithm:
-		 * - calculates rate value, applying the 'rater' to specified 'value'
-		 * - makes the decision: 
-		 *    - if rate < currentMax then do nothing
-		 *    - if rate == currentMax then collect
-		 *    - if rate > currentMax then make it new champion
-		 * @param value - next element from stream
-		 */
 		public void accumulate(T value) {
 			long rate = rater.applyAsLong(value);
 			if(currentWinnersList.isEmpty()) {
@@ -74,22 +40,20 @@ public class MaxRatedWinnersCollector {
 			}
 			currentWinnersList.add(value);
 		}
-
-		/**
-		 * Updates this accumulator using data from other:
-		 * - if one of rates are greater, then only greater remains
-		 * - else this appends other's list of champions to it's own list
-		 * @param other - other accumulator
-		 * @return this accumulator
-		 */
 		public RateAccumulator<T> combine(RateAccumulator<T> other) {
 			if(this.currentMax == other.currentMax) {
 				this.currentWinnersList.addAll(other.currentWinnersList);
 			}
 			if(this.currentMax < other.currentMax){
-				return other;
-			}			
+				this.currentMax = other.currentMax;
+				this.currentWinnersList = other.currentWinnersList;
+			}
 			return this;
 		}
+		public List<T> res() {
+			return currentWinnersList;			
+		}
 	}
+	
+	
 }
