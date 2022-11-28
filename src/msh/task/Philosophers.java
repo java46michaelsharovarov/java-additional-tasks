4fdfe7564c1af8795ec13c04e0b9466516393d98
Mik <m.shac86@gmail.com>
2022-11-28 17:53:15 +0200

    task-42 final solution

--- src/msh/task/Philosophers.java
+++ src/msh/task/Philosophers.java
@@ -1,63 +1,64 @@
 package msh.task;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Philosophers {
-    public static void main(String[] args) throws Exception {
-    	
-        Philosopher[] philosophers = new Philosopher[5];
-        ReentrantLock[] forks = new ReentrantLock[philosophers.length];
-        
-        for (int i = 0; i < forks.length; i++) {
-            forks[i] = new ReentrantLock();
-        }
-        for (int i = 0; i < philosophers.length; i++) {
-        	ReentrantLock leftFork = forks[i];
-        	ReentrantLock rightFork = forks[(i + 1) % forks.length];
-            philosophers[i] = new Philosopher(leftFork, rightFork);
-            Thread t = new Thread(philosophers[i], "Philosopher " + (i + 1));
-            t.start();
-        }
-    }
-    static class Philosopher implements Runnable {
+	public static void main(String[] args) throws Exception {
 
-        private final ReentrantLock leftFork;
-        private final ReentrantLock rightFork;
+		Philosopher[] philosophers = new Philosopher[5];
+		Object[] forks = new Object[philosophers.length];
 
-        public Philosopher(ReentrantLock leftFork, ReentrantLock rightFork) {
-            this.leftFork = leftFork;
-            this.rightFork = rightFork;
-        }
+		for (int i = 0; i < forks.length; i++) {
+			forks[i] = new Object();
+		}
+		
+		philosophers[0] = new Philosopher(forks[1], forks[0]);
+		Thread t = new Thread(philosophers[0], "Philosopher " + 1);
+		t.start();
+		
+		for (int i = 1; i < philosophers.length; i++) {
+			Object leftFork = forks[i];
+			Object rightFork = forks[(i + 1) % forks.length];
+			philosophers[i] = new Philosopher(leftFork, rightFork);
+			t = new Thread(philosophers[i], "Philosopher " + (i + 1));
+			t.start();
+		}
+	}
 
-        private void doAction(String action) throws InterruptedException {
-            System.out.println(Thread.currentThread().getName() + " " + action);
-            Thread.sleep(((int) (Math.random() * 10)));
-        }
+	static class Philosopher implements Runnable {
 
-        @Override
-        public void run() {
-            try {
-                while (true) {
-                    doAction(System.nanoTime() + ": Thinking");
-                    leftFork.tryLock();
-                    try {
-						doAction(System.nanoTime() + ": Picked up left fork");                        
+		private final Object leftFork;
+		private final Object rightFork;
+
+		public Philosopher(Object leftFork, Object rightFork) {
+			this.leftFork = leftFork;
+			this.rightFork = rightFork;
+		}
+
+		private void doAction(String action) throws InterruptedException {
+			System.out.println(Thread.currentThread().getName() + " " + action);
+			Thread.sleep(((int) (Math.random() * 10)));
+		}
+
+		@Override
+		public void run() {
+			try {
+				while (true) {
+					// thinking
+					doAction(System.nanoTime() + ": Thinking");
+					synchronized (leftFork) {
+						doAction(System.nanoTime() + ": Picked up left fork");
+						synchronized (rightFork) {
+							// eating
+							doAction(System.nanoTime() + ": Picked up right fork - eating");
+
+							doAction(System.nanoTime() + ": Put down right fork");
+						}
+						// Back to thinking
 						doAction(System.nanoTime() + ": Put down left fork. Back to thinking");
-					} finally {
-						 leftFork.unlock();
-					} 
-                    rightFork.tryLock();
-                    try {
-						doAction(System.nanoTime() + ": Picked up right fork - eating");
-						doAction(System.nanoTime() + ": Put down right fork");
-					} finally {
-						rightFork.unlock();
-					} 
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
+					}
+				}
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
 }
-
